2456b8d1b97b1f627029d34071b3777e721756c4
laurensvanheems <laurens@your-majesty.com>
2015-02-19 15:20:06 -0500

    Adds a working prototype

--- app.js
+++ app.js
@@ -1,33 +1,110 @@
+/* Node requirements */
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/src'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/src/views/index.html');
-});
+/* Game Vars */
 
-io.on('connection', function(client){
+var players = [];
 
-  client.on('join', function(name){
-    client.nickname = name;
-    client.emit('new player', name);
-    client.broadcast.emit('new player', name);
+
+/* Game Initialisation */
+
+function init() {
+
+  app.use(express.static(__dirname + '/src'));
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/src/views/index.html');
   });
 
-  client.on('add move', function(data){
-    var move = {name: client.nickname, key: data};
-    client.broadcast.emit('add move', move);
-    client.emit('add move', move);
-  });
+  server.listen(8080);
 
-  client.on('user disconnect', function(){
-    client.broadcast.emit('user disconnect');
-    client.emit('user disconnect');
-  });
+  setEventHandlers();
 
-});
+}
 
-server.listen(8080);
+/* Game events */
+
+function setEventHandlers() {
+  io.on('connection', onSocketConnection);
+}
+
+function onSocketConnection(client) {
+
+  client.on('join', onPlayerJoin);
+
+  client.on('add move', onPlayerMove);
+
+  client.on('disconnect', onPlayerDisconnect);
+
+}
+
+function onPlayerJoin(name) {
+  var newPlayer = {id: this.id, name: name, role: 0},
+      readyToStart = false;
+
+  if(players.length === 1) {
+    newPlayer.role = 1;
+  }
+
+  players.push(newPlayer);
+  if(players.length === 2) {
+    readyToStart = true;
+  }
+
+  this.emit('player', newPlayer, readyToStart);
+  this.broadcast.emit('new player', readyToStart);
+}
+
+function onPlayerMove(data) {
+  if(players.length !== 2) {
+    return;
+  }
+
+  var move = {id: this.id, key: data, role: playerById(this.id).role};
+
+  this.emit('add move', move);
+  this.broadcast.emit('add move', move);
+
+}
+
+function onPlayerDisconnect() {
+  console.log('lost connection');
+
+  var removePlayer = playerById(this.id);
+
+  // Player not found
+  if (!removePlayer) {
+    console.log("Player not found: "+this.id);
+    return;
+  };
+
+  // Remove player from players array
+  players.splice(players.indexOf(removePlayer), 1);
+
+  // Broadcast removed player to connected socket clients
+  if(players.length !== 2) {
+    this.broadcast.emit('end game');
+  }
+
+}
+
+
+/* Helper functions */
+
+function playerById(id) {
+  var i;
+  for (i = 0; i < players.length; i++) {
+    if (players[i].id == id)
+      return players[i];
+  };
+
+  return false;
+};
+
+/* Start Game */
+init();
--- src/assets/javascripts/client.js
+++ src/assets/javascripts/client.js
@@ -1,77 +1,139 @@
-$(function(){
+var server = io.connect(),
+    moves = [],
+    responseTime = 1000,
+    range = 200,
+    player;
 
-  var server = io.connect();
+function init() {
+  server.on('connect', onSocketConnect);
+  server.on('player', onPlayer);
+  server.on('new player', onNewPlayer);
+  server.on('start game', onStartGame);
+  server.on('end game', onEndGame);
+  server.on('add move', onNewMove);
+}
 
-  // Count players on server, not on client!
-  var gameStarted = false,
-      amountPlayers = 0;
+function onSocketConnect() {
+  console.log('Server connected');
 
-  server.on('connect', function(data){
+  $('.name').on('submit', function(e){
+    e.preventDefault();
+    var name = $('.name__input').val();
 
-    $('.name').on('submit', function(e){
-      e.preventDefault();
-      var name = $('.name__input').val();
+    if(name == '') {
+      $('.name__input').addClass('error');
+      return;
+    }
 
-      if(name == '') {
-        $('.name__input').addClass('error');
-        return;
-      }
-
-      server.emit('join', name);
-      $('.view').removeClass('active');
-      $('.view[data-view="game"]').addClass('active');
-    });
-
-    server.on('new player', function(data){
-      console.log(data);
-      amountPlayers++;
-      if(amountPlayers === 2) {
-        gameStarted = true;
-      }
-    });
-
-    server.on('add move', function(data){
-      console.log(data);
-      var key;
-      switch (data.key) {
-        // Controls
-        case 37: // Left
-          key = 'left';
-          break;
-        case 38: // Up
-          key = 'up';
-          break;
-        case 39: // Right
-          key = 'right';
-          break;
-        case 40: // Down
-          key = 'down';
-          break;
-      };
-      $('.objects').append(key + '<br/>');
-    });
-
-    server.on('disconnect', function(){
-      server.emit('user disconnect');
-    });
-
-    server.on('user disconnect', function(){
-      amountPlayers--;
-      gameStarted = false;
-    });
-
+    server.emit('join', name);
+    $('.view').removeClass('active');
+    $('.view[data-view="wait"]').addClass('active');
   });
-
 
   $('.name__input').on('keydown', function(){
     $(this).removeClass('error');
   });
 
   $(document).on('keydown', function(e){
-    if (gameStarted === true) {
-      server.emit('add move', e.keyCode);
-    }
+    server.emit('add move', e.keyCode);
   });
 
+}
+
+function onPlayer(data, start) {
+  player = data;
+  console.log(player);
+
+  $('.view').removeClass('active');
+  if(start === true) {
+    $('.view[data-view="game"]').addClass('active');
+  } else {
+    $('.view[data-view="wait"]').addClass('active');
+  }
+}
+
+function onNewPlayer(start) {
+  if(player) {
+    $('.view').removeClass('active');
+    if(start === true) {
+      $('.view[data-view="game"]').addClass('active');
+    } else {
+      $('.view[data-view="wait"]').addClass('active');
+    }
+  }
+}
+
+function onStartGame(data) {
+  $('.view').removeClass('active');
+  $('.view[data-view="game"]').addClass('active');
+}
+
+function onEndGame(data) {
+  if(!$('.view[data-view="start"]').hasClass('active')) {
+    $('.view').removeClass('active');
+    $('.view[data-view="wait"]').addClass('active');
+  }
+}
+
+function onNewMove(data) {
+
+  console.log(data);
+  var key;
+  switch (data.key) {
+    // Controls
+    case 37: // Left
+      key = 'left';
+      break;
+    case 38: // Up
+      key = 'up';
+      break;
+    case 39: // Right
+      key = 'right';
+      break;
+    case 40: // Down
+      key = 'down';
+      break;
+    default:
+      return;
+  };
+
+  if(data.role === 0) {
+    onLeaderMove(key);
+  } else {
+    onFollowerMove(key);
+  }
+}
+
+function onLeaderMove(key) {
+  var moveTime = Date.now() + responseTime,
+      moveHtml = $('<figure class="key" data-time="'+moveTime+'">'+key+'</figure>');
+
+  console.log(moveTime);
+  moves.push({time: moveTime, key: key});
+
+  $('.objects').append(moveHtml);
+
+  setTimeout(function(){
+    moveHtml.remove();
+    console.log(moves[0]);
+    moves.splice(0, 1);
+  },2000);
+}
+
+function onFollowerMove(key) {
+  var followerTime = Date.now(),
+      moveTime = moves[0].time,
+      moveDiff = Math.abs(followerTime - moveTime);
+
+  console.log(moveDiff);
+
+  if(moveDiff < range && key == moves[0].key) {
+    $('.key[data-time="'+moveTime+'"]').addClass('green');
+  }
+
+}
+
+$(function(){
+  init();
 });
 
