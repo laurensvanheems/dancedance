48a983b1205231b85418154c03d1757edc6981ec
laurensvanheems <laurens@your-majesty.com>
2015-02-18 19:52:47 -0500

    Start of Dance Dance game

--- app.js
+++ app.js
@@ -2,58 +2,25 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var mongoose = require('mongoose');
-var Message;
-
-mongoose.connect('mongodb://localhost/test');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-
-  var messageSchema = mongoose.Schema({
-    name: String,
-    data: String
-  });
-
-  Message = mongoose.model('Message', messageSchema);
-
-});
 
 app.use(express.static(__dirname + '/src'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/src/views/index.html');
 });
 
-var messages = [];
-
-var storeMessages = function(name, data){
-  var message = new Message({ name: name, data: data });
-  message.save(function (err, message) {
-    if (err) return console.error(err);
-  });
-}
-
 io.on('connection', function(client){
 
   client.on('join', function(name){
-    Message.find(function (err, messages) {
-      if (err) return console.error(err);
-      messages.forEach(function(message){
-        client.emit('messages', message.name + ': ' + message.data);
-      });
-    });
-
     client.nickname = name;
-    client.broadcast.emit('messages', name + ' just joined');
+    client.emit('new player', name);
+    client.broadcast.emit('new player', name);
   });
 
-  client.on('messages', function(data){
-
-    var nickname = client.nickname;
-    client.broadcast.emit('messages', nickname + ': ' + data);
-    client.emit('messages', nickname + ': ' + data);
-    storeMessages(nickname, data);
+  client.on('add move', function(data){
+    var move = {name: client.nickname, key: data};
+    client.broadcast.emit('add move', move);
+    client.emit('add move', move);
   });
 
 });
--- src/assets/javascripts/client.js
+++ src/assets/javascripts/client.js
@@ -1,20 +1,66 @@
-var server = io.connect('http://localhost:8080');
+$(function(){
 
-server.on('connect', function(data){
+  var server = io.connect();
 
-  nickname = prompt('Nickname');
-  server.emit('join', nickname);
+  var gameStarted = false,
+      amountPlayers = 0;
 
-  $('.form').on('submit', function(e){
-    e.preventDefault();
-    server.emit('messages', $('.input').val());
-    $('.input').val('');
+  server.on('connect', function(data){
+
+    $('.name').on('submit', function(e){
+      e.preventDefault();
+      var name = $('.name__input').val();
+
+      if(name == '') {
+        $('.name__input').addClass('error');
+        return;
+      }
+
+      server.emit('join', name);
+      $('.view').removeClass('active');
+      $('.view[data-view="game"]').addClass('active');
+    });
+
+    server.on('new player', function(data){
+      console.log(data);
+      amountPlayers++;
+      if(amountPlayers === 2) {
+        gameStarted = true;
+      }
+    });
+
+    server.on('add move', function(data){
+      console.log(data);
+      var key;
+      switch (data.key) {
+        // Controls
+        case 37: // Left
+          key = 'left';
+          break;
+        case 38: // Up
+          key = 'up';
+          break;
+        case 39: // Right
+          key = 'right';
+          break;
+        case 40: // Down
+          key = 'down';
+          break;
+      };
+      $('.objects').append(key + '<br/>');
+    });
+
   });
 
-  server.on('messages', function(data){
+  $('.name__input').on('keydown', function(){
+    $(this).removeClass('error');
+  });
 
-    $('.messages').append(data + '<br/>');
-
+  $(document).on('keydown', function(e){
+    if (gameStarted === true) {
+      server.emit('add move', e.keyCode);
+    }
   });
 
 });
+
